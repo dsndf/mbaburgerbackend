b3362168ee4325774dec45f7795b19a38ab17861
Gaurav Jain <134189121+dsndf@users.noreply.github.com>
2023-05-31 18:45:48 +0530

    Update userAuth.js

--- src/middleware/userAuth.js
+++ src/middleware/userAuth.js
@@ -4,7 +4,9 @@ import errorHandler from "../utils/ErrorHandler.js";
 
  const userAuth = async (req,res,next)=>{
    const token =  req.cookies['connect.sid'];
-  console.log("this is token "+token);
+    console.log("this is token "+token);
+  console.log("this is user "+req.user);
+  req.me =  req.user;
     if(!token){
      next(errorHandler("Please log in"));       
     }
